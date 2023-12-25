8acdb33b84a7dfb4d47b5864ec6c4e71cef7d895
DelvinoArdi77 <delvino.ardy88@gmail.com>
2023-12-25 18:08:46 +0700

    add new 4

--- app/src/main/java/com/example/kaguapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/kaguapp/DashboardAdminActivity.kt
@@ -3,14 +3,22 @@ package com.example.kaguapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.kaguapp.databinding.ActivityDashboardAdminBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashboardAdminActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityDashboardAdminBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var adapterCategory: AdapterCategory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
@@ -18,6 +26,19 @@ class DashboardAdminActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        binding.searchEt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapterCategory.filter.filter(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
@@ -28,6 +49,27 @@ class DashboardAdminActivity : AppCompatActivity() {
             startActivity(Intent(this, CategoryAddActivity::class.java))
         }
 
+    }
+
+    private fun loadCategories() {
+        categoryArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance("https://kaguribook-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Categories")
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                categoryArrayList.clear()
+                for(ds in snapshot.children){
+                    val model = ds.getValue(ModelCategory::class.java)
+
+                    categoryArrayList.add(model!!)
+                }
+                adapterCategory = AdapterCategory(this@DashboardAdminActivity, categoryArrayList)
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 
     private fun checkUser() {
--- app/src/main/java/com/example/kaguapp/DashboardUserActivity.kt
+++ app/src/main/java/com/example/kaguapp/DashboardUserActivity.kt
@@ -3,13 +3,21 @@ package com.example.kaguapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.kaguapp.databinding.ActivityDashboardUserBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class DashboardUserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardUserBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var adapterCategory: AdapterCategory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardUserBinding.inflate(layoutInflater)
@@ -17,6 +25,19 @@ class DashboardUserActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        loadCategories()
+
+        binding.searchEt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapterCategory.filter.filter(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
         binding.logoutBtn.setOnClickListener {
             firebaseAuth.signOut()
@@ -25,6 +46,26 @@ class DashboardUserActivity : AppCompatActivity() {
         }
     }
 
+    private fun loadCategories() {
+        categoryArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance("https://kaguribook-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Categories")
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                categoryArrayList.clear()
+                for(ds in snapshot.children){
+                    val model = ds.getValue(ModelCategory::class.java)
+
+                    categoryArrayList.add(model!!)
+                }
+                adapterCategory = AdapterCategory(this@DashboardUserActivity, categoryArrayList)
+                binding.categoriesRv.adapter = adapterCategory
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
     private fun checkUser() {
         val firebaseUser = firebaseAuth.currentUser
         if(firebaseUser == null){
